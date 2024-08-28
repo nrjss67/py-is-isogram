ac7babebfe3ec5e4eb186c7d47e5e4941bc8d086
Yaroslav Ivanov <extazzzyprofit@gmail.com>
2024-08-28 17:56:01 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,31 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,expected",
+    [
+        ("", True),
+        ("python", True),
+        ("smart", True),
+        ("background", True),
+        ("look", False),
+        ("froNtend", False),
+        ("backend", True),
+        ("Paprica", False)
+    ]
+)
+def test_with_different_attributes(word, expected): # noqa
+    assert is_isogram(word) == expected
+
+
+@pytest.mark.parametrize(
+    "word,expected",
+    [
+        (123, AttributeError),
+        ([1], AttributeError)
+    ]
+)
+def test_with_incorrect_value_type(word, expected): # noqa
+    with pytest.raises(expected):
+        is_isogram(word)
